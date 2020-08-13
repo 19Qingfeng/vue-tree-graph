4641123d075ffd121ce09e2bd121ff75b68cc020
王浩羽 <wanghaoyu@wanghaoyudeMBP.lan>
2020-08-13 21:51:49 +0800

    强烈耦合业务，增加结尾

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,52 @@
 <template>
-  <div id="app">
-    <label>
-      <!-- 切换为横向 -->
-      <input type="checkbox" v-model="landscape" value="1">
-    </label>
-    <TreeChart :json="data" :class="{landscape: landscape.length}" @click-node="clickNode" />
-    
-  </div>
+    <div id="app">
+        <TreeChart
+            :json="data"
+            :level="getTreeDepth(data)"
+            :id="getMax(data)"
+            :class="{landscape: landscape.length}"
+            @click-node="clickNode"
+            :height="174"
+        />
+    </div>
 </template>
 
 <script>
 import TreeChart from "@/components/TreeChart";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     TreeChart
   },
   data() {
     return {
       landscape: [],
+      mockId: 10000,
       data: {
-        name: 'root',
-        title:"发起申请",
-        body:"发起人",
+        name: "root",
+        id: "1",
+        title: "发起申请",
+        body: "发起人",
+        level: 1,
         image_url: "https://static.refined-x.com/static/avatar.jpg",
         class: ["rootNode"],
+        node: null,
         children: [
           {
-            name: 'children1',
-            title:"分支1",
-            body:"请假时长<=8小时",
-            image_url: "https://static.refined-x.com/static/avatar.jpg"
+            name: "children1",
+            id: "2",
+            title: "分支1",
+            level: 2,
+            body: "请假时长<=8小时",
+            image_url: "https://static.refined-x.com/static/avatar.jpg",
           },
           {
-            name: 'children2',
-            title:"分支2",
-            body:"请假时长很长",
+            name: "children2",
+            title: "分支2",
+            id: "3",
+            level: 2,
+            body: "请假时长很长",
             image_url: "https://static.refined-x.com/static/avatar.jpg",
             // mate: [
             //   {
@@ -46,87 +56,125 @@ export default {
             // ],
             children: [
               {
-                name: 'grandchild',
-                title:"分支x",
-                body:"请假时长", 
-                image_url: "https://static.refined-x.com/static/avatar.jpg"
+                name: "grandchild",
+                title: "分支x",
+                id: 4,
+                level: 3,
+                body: "请假时长",
+                image_url: "https://static.refined-x.com/static/avatar.jpg",
               },
               {
-                name: 'grandchild2',
-                title:"分支9",
-                body:"xxx",
-                image_url: "https://static.refined-x.com/static/avatar.jpg"
+                name: "grandchild2",
+                title: "分支9",
+                level: 3,
+                id: 5,
+                body: "xxx",
+                image_url: "https://static.refined-x.com/static/avatar.jpg",
               },
               {
-                name: 'grandchild3',
-                title:"分支yy",
-                body:"999",
-                image_url: "https://static.refined-x.com/static/avatar.jpg"
-              }
-            ]
-          }
-        ]
-      }
-    }
+                name: "grandchild3",
+                level: 3,
+                id: 6,
+                title: "分支yy",
+                body: "999",
+                image_url: "https://static.refined-x.com/static/avatar.jpg",
+              },
+            ],
+          },
+        ],
+      },
+    };
   },
   methods: {
-    clickNode: function(node){
-      // eslint-disable-next-line
-      // const length = children.length ? children.length : 
-      console.log(node,'node')
+    // 获得最后节点 取消border的id
+    getMax(data) {
+      let node = null;
+      function each(data) {
+        const length = data.length - 1;
+        for (let [key, value] of data.entries()) {
+          if (key === length) {
+            if (value.children && value.children.length) {
+              // 继续调用
+              each(value.children);
+            } else {
+              node = value;
+            }
+          }
+        }
+      }
+      each(data.children);
+      return node.id;
+    },
+    getTreeDepth(treeData) {
+      let max = 0;
+      treeData = [treeData];
+      function each(data, floor) {
+        data.forEach((node) => {
+          if (floor > max) {
+            max = floor;
+          }
+          if (node.children && node.children.length > 0) {
+            each(node.children, floor + 1);
+          }
+        });
+      }
+      each(treeData, 1);
+      return max;
+    },
+    clickNode(node) {
       let children;
-      if(node.children) {
-        children = node.children
-      }else {
+      if (node.children) {
+        children = node.children;
+      } else {
         children = [];
-        this.$set(node,'children',children)
+        this.$set(node, "children", children);
       }
-      console.log(children,'children')
-      const key = children&&children.length ? children.length : 0;
-      // debugger
-      console.log(key,'key')
-      const value = {
-        name:"add",
-        title:"新增加",
-        body:"success"
-      }
-      this.$set(children,key,{
-        name:"add",
-        title:"新增加",
-        body:"success"
-      })
-      console.log(children,'children')
-    }
-  }
-}
+      const key = children && children.length ? children.length : 0;
+      this.$set(children, key, {
+        name: "add",
+        title: "新增加",
+        body: "success",
+        level: node.level + 1,
+        id: this.mockId--,
+      });
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
-#app .avat{border-width:2px;}
-#app .name{font-weight:700;}
-#app .rootNode .name{
+#app .avat {
+  border-width: 2px;
+}
+#app .name {
+  font-weight: 700;
+}
+#app .rootNode .name {
   color: red;
 }
 
 .foot {
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    background: #333;
-    padding: 24px;
-    overflow: hidden;
-    color: #999;
-    font-size: 14px;
-    text-align: center;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background: #333;
+  padding: 24px;
+  overflow: hidden;
+  color: #999;
+  font-size: 14px;
+  text-align: center;
 }
-.foot a{color:#fff;margin:0 .5em}
+.foot a {
+  color: #fff;
+  margin: 0 0.5em;
+}
 </style>
---
+++ src/components/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div class="wapper">
+            <div class="row">
+                <div class="col">
+                    <div class="title">title</div>
+                    <div class="body">body</div>
+                    
+                </div>
+            </div>
+            <div class="row">
+
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:"TreeMap",
+    props:{
+        treeData:Object
+    },
+    data() {
+        return {}
+    }
+}
+</script>
+<style>
+    .wapper {
+        display:flex;
+        text-align: center;
+    }
+    .title{
+        background-color: #eee;
+        width:100px;
+        line-height: 30px;
+        height: 30px;
+    }
+    .body{
+        background-color: #777;
+        width: 100px;
+        height: 60px;
+        line-height: 60px;
+        position: relative;
+    }
+    .body::after{
+        content: "";position: absolute;left:0;right:0;top:-15px;border-top:2px solid #ccc;
+    }
+</style>
